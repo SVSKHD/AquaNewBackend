d005b13206327a144865a1f0d05a3ac6825ccbb4
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2022-06-05 15:59:32 +0530

    new chnages in aquakart backend

--- Crm/Controllers/user.js
+++ Crm/Controllers/user.js
@@ -46,45 +46,47 @@ exports.Signup =  async (req, res) => {
   }
 
 exports.Login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    if (!(email && password)) {
-      res.status(400).send("Field is missing");
+    try {
+      const { email, password } = req.body;
+  
+      if (!(email && password)) {
+        res.status(400).send("Field is missing");
+      }
+  
+      const user = await crmUser.findOne({ email });
+  
+      // if(!user){
+      //   res.status(400).send("You are not registered in our app")
+      // }
+  
+      if (user && (await bcrypt.compare(password, user.password))) {
+        const token = jwt.sign(
+          { user_id: user._id, email },
+          process.env.SECRET_KEY,
+          {
+            expiresIn: "2h",
+          }
+        );
+        user.token = token;
+        user.password = undefined;
+        // res.status(200).json(user);
+  
+        // if you want to use cookies
+        const options = {
+          expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+          httpOnly: true,
+        };
+  
+        res.status(200).cookie("token", token, options).json({
+          success: true,
+          token,
+          user,
+        });
+      }else{
+        res.sendStatus(400).send("email or password is incorrect");
+      }
+    } catch (error) {
+      console.log(error);
     }
-
-    const user = await crmUser.findOne({ email });
-
-    // if(!user){
-    //   res.status(400).send("You are not registered in our app")
-    // }
-
-    if (user && (await bcrypt.compare(password, user.password))) {
-      const token = jwt.sign(
-        { user_id: user._id, email },
-        process.env.SECRET_KEY,
-        {
-          expiresIn: "2h",
-        }
-      );
-      user.token = token;
-      user.password = undefined;
-      // res.status(200).json(user);
-
-      // if you want to use cookies
-      const options = {
-        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
-      };
-
-      res.status(200).cookie("token", token, options).json({
-        success: true,
-        token,
-        user,
-      });
-    }
-
-    res.sendStatus(400).send("email or password is incorrect");
-  } catch (error) {
-    console.log(error);
   }
-}
+
